06bd056c7fac5ff3e8386f09b55c6ebe43f6965a
Fabiany Lopes <53617138+fabianylopes@users.noreply.github.com>
2022-05-04 07:47:37 -0300

    Update style.js

--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -3,18 +3,42 @@ import { Link } from "react-router-dom";
 
 const Title = styled.h1`
     font-family: 'Saira Stencil One', cursive;
+    font-weight: 400;
+    font-size: 32px;
+    line-height: 50px;
 `
 
 const Input = styled.input`
-
+    width: 326px;
+    height: 58px;
+    border-radius: 5px;
+    outline: 0;
+    padding-left: 15px;
+   
+    ::placeholder{
+        color: #000;
+        font-family: 'Raleway';
+        font-size: 20px;
+        font-weight: 400;
+    }
 `
 
 const Button = styled.button`
-
+    background-color: #A328D6;
+    color: #fff;
+    width: 326px;
+    height: 46px;
+    border: none;
+    border-radius: 5px;
+    font-size: 20px;
+    font-weight: 700;
 `
 
 const StyledLink = styled(Link)`
-
+    font-family: 'Raleway';
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 18px;
 `
 
 export { 
