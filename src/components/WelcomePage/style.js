200f3714786aefe29effb94cd1b99b4a821e8e1f
Fabiany Lopes <53617138+fabianylopes@users.noreply.github.com>
2022-05-04 07:54:32 -0300

    Update style.js

--- src/components/WelcomePage/style.js
+++ src/components/WelcomePage/style.js
@@ -1,15 +1,22 @@
 import styled from 'styled-components';
 
 const Top = styled.div`
-
+    display: flex;
+    justify-content: space-between;
 `
 
 const Title = styled.h1`
-
+    color: #fff:
+    font-family: 'Raleway';
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 31px;
 `
 
 const Container = styled.div`
-
+    width: 326px;
+    height: 446px;
+    border-radius: 5px;
 `
 
 const Footer = styled.div`
@@ -17,10 +24,18 @@ const Footer = styled.div`
 `
 
 const Transaction = styled.div`
-
+    width: 155px;
+    height: 114px;
+    border-radius: 5px;
+    background-color: #A328D6;
 `
 
 const Text = styled.h2`
+    color: #fff;
+    font-family: 'Raleway';
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
 `
 
 export { 
